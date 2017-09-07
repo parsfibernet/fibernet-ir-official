ebdfa508e6e20a9b1236e825f25150305ccd1766
Mehdi Moshtaghi <mehdi.mm@gmail.com>
2017-09-07 09:19:29 +0430

    setup angular application

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+'use strict';
 // --------------------------------------------------
 // APP.JS
 // --------------------------------------------------
@@ -21,5 +22,71 @@ $(window).bind("load", function () {
         footer.css({
             'margin-top': height + 'px'
         });
+    }
+});
+
+//
+// Angular App
+// --------------------------------------------------
+var app = angular.module('fast-charge', ['ngRoute', 'angular-loading-bar']);
+
+// Angular Routing
+// ---------------
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : 'login.html',
+      controller : 'mainCtrl'
+    })
+    .when("/step-2", {
+      templateUrl : "step-2.html",
+      controller : 'mainCtrl'
+    });
+  });
+// Bind Access Token
+// -----------------
+app.factory('httpRequestInterceptor',function () {
+    return {
+        request: function (config) {
+            config.headers['Authorization'] = 'Bearer Rr3R8QDjkqz5hZ1MZGE7aTqx0ajc7SmcKlr03Xxv';
+            return config;
+        }
+    };
+})
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+})
+// Main Controller
+// ---------------
+app.controller('mainCtrl', ($scope, $http, $location) => {
+    function getProducts(cid, pid, type, phone) {
+        let url = `https://core.fibernet.ir/2.0/web/products/city/${cid}/provider/${pid}/types/${type}?phone=${phone}`;
+        return $http.get(url);
+    }
+    $scope.fetchAllAvailableProducts = (phone, mobile) => {
+        var code = phone.toString().substring(0,2);
+        switch (code){
+            case '11':
+                var city = 8;
+                var provider = 1;
+                break;
+            case '17':
+                var city = 1;
+                var provider = 3;
+                break;
+            case '83':
+                var city = 29;
+                var provider = 4;
+                break;
+        }
+        getProducts(city, provider, 'ADSL', phone).then(
+            (response) => {
+                console.log('SUCCESS : ', response);
+                $location.path('step-2');
+            },
+            (error) => {
+                console.log('ERROR : ', error);
+            }
+        );
     }
 });
